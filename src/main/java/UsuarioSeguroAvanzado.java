bb700065d2cb2024bf80da91cb6a46e1f72ddbcb
Laboratorio <laboratorio@udla-ec.int>
2026-04-22 10:52:17 -0500

    Miguel torres v1

---
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -0,0 +1,20 @@
+public class UsuarioSeguroAvanzado {
+
+    private String username;
+    private String password;
+    private int intentosFallidos;
+    private boolean bloqueado;
+    private int maxIntentos;
+    private boolean accesoExitoso;
+
+    public UsuarioSeguroAvanzado(String username, String password, int maxIntentos) {
+        this.username = username;
+        this.password = password;
+        this.intentosFallidos = 0;
+        this.bloqueado = false;
+        this.accesoExitoso = false;
+        this.maxIntentos = (maxIntentos <= 0) ? 3 : maxIntentos;
+    }
+
+ 
+}
